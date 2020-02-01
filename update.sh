67d443eadfe37979192d11a9b51bc805944f2e6b
awright <awright@freelancer.com>
2020-02-01 20:18:02 +1100

    Use home.nix instead of specific machine in update helper script

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ set -e
 source $HOME/.nix-profile/etc/profile.d/nix.sh
 nix-channel --update
 export NIX_PATH=$HOME/.nix-defexpr/channels${NIX_PATH:+:}$NIX_PATH
-home-manager switch -f machines/work.nix
+home-manager switch -f home.nix
 vim -E -c UpdateRemotePlugins -c q
