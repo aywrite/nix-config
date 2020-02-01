37388305cc77b1321d2485fa63ae656aebc7c20c
awright <awright@freelancer.com>
2020-02-01 21:09:51 +1100

    Manage global gitignores with nix

--- users/base.nix
+++ users/base.nix
@@ -18,4 +18,33 @@
     findutils
     gnugrep
   ];
+
+  # default git ignores
+  programs.git = {
+    ignores = [
+      # Git credential file
+      ".gitconfig.local"
+      # Folder view configuration files
+      ".DS_Store"
+      "Desktop.ini"
+     # Thumbnail cache files
+     "._*"
+     "*~"
+     "Thumbs.db"
+     # Files that might appear on external disks
+     ".Spotlight-V100"
+     ".Trashes"
+     # Compiled Python files
+     "*.pyc"
+     ".mypy_cache/"
+     # npm & bower
+     "bower_components"
+     "node_modules"
+     "npm-debug.log"
+     # IDEs stuff
+     ".idea"
+     # haskell
+     ".o"
+    ];
+  };
 }
