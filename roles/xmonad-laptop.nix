905de2feeb8a1b0f9eb32aac8dfa35e4ee21a5a6
awright <awright@freelancer.com>
2020-02-08 11:12:27 +1100

    Manage xmonad and xorg config with home manager

--- roles/xmonad-laptop.nix
+++ roles/xmonad-laptop.nix
@@ -7,17 +7,31 @@
   ];
   nixpkgs.config.allowUnfree = true;
 
-  # TODO move xmonad under home manager properly
-  # Can't do the whole xmonad directory because it prevents
-  # xmonad for outputing the build and error files etc.
-  home.file.".xmonad/xmonad.hs".source = ../programs/xmonad/xmonad.hs;
-  home.file.".xmobarrc".source = ../programs/status-bars/xmobarrc;
+  home.file.".xinitrc".source = ../programs/xorg/xinitrc;
+  home.file.".profile".source = ../programs/xorg/xprofile;
+  xresources.extraConfig = builtins.readFile ../programs/xorg/Xresources;
+  xsession.enable = true;
+
+  # TODO move this to user?
+  programs.urxvt = {
+    enable = true;
+  };
 
   programs.alacritty = {
     enable = true;
     settings = lib.attrsets.recursiveUpdate (import ../programs/alacritty.nix) {
       shell.program = "zsh";
     };
+  };
+
+  # Can't do the whole xmonad directory because it prevents
+  # xmonad for outputing the build and error files etc.
+  home.file.".xmonad/xmonad.hs".source = ../programs/xmonad/xmonad.hs;
+  home.file.".xmobarrc".source = ../programs/status-bars/xmobarrc;
+
+  xsession.windowManager.xmonad = {
+    enable = true;
+    enableContribAndExtras = true;
   };
 
   # Environment
