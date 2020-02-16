841d7f8018c56243612a10b25b2fb0b2098a0862
awright <anderw.w@covergenius.com>
2020-02-16 20:55:01 +1100

    Add cover genius variant of user for awright
    For use on work devices at cover genius.

---
+++ users/awright-cg.nix
@@ -0,0 +1,36 @@
+{ config, pkgs, ... }:
+
+{
+  imports = [
+    ../users/base.nix
+    ../programs/bash.nix
+    ../programs/neovim.nix
+    ../programs/tmux/default.nix
+  ];
+
+  home.packages = with pkgs; [
+    # dev ops
+    kubectl
+    google-cloud-sdk
+  ];
+
+  programs.git = {
+    enable = true;
+    userEmail = "anderw.w@covergenius.com";
+    userName = "awright";
+    signing.key = "0x46BAAEE2AFADB938";
+    signing.signByDefault = true;
+    extraConfig = {
+      url = {
+        "git@github.com:" = {
+          insteadOf = "https://github.com/";
+        };
+      };
+      url = {
+        "git@bitbucket.com:" = {
+          insteadOf = "https://bitbucket.com/";
+        };
+      };
+    };
+  };
+}
