011f966b2f6ebaf5ed46098f6fa5b87ea4d4a879
Andrew Wright <anderw.w@covergenius.com>
2020-03-30 12:30:24 +1100

    Set proxy variables for work VPN

--- users/awright-cg.nix
+++ users/awright-cg.nix
@@ -20,6 +20,12 @@
 
   home.sessionVariables = {
     BROWSER = "ff-work";
+    http_proxy = "http://10.255.254.1:8118";
+    https_proxy = "http://10.255.254.1:8118";
+    HTTP_PROXY = "http://10.255.254.1:8118";
+    HTTPS_PROXY = "http://10.255.254.1:8118";
+    no_proxy = "localhost,.rentalcover.internal,.xcover.internal,.brightwrite.internal,127.0.0.1/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,224.0.0.0/4,223.255.255.0/24,192.0.0.0/24,191.255.0.0/16,128.0.0.0/8";
+    NO_PROXY = "localhost,.rentalcover.internal,.xcover.internal,.brightwrite.internal,127.0.0.1/8,10.0.0.0/8,172.16.0.0/12,192.168.0.0/16,224.0.0.0/4,223.255.255.0/24,192.0.0.0/24,191.255.0.0/16,128.0.0.0/8";
   };
 
   programs.git = {
