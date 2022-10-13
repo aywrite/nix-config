3766e84ff9f1cff208ffb69981dc8ffa51a6d636
Andrew Wright <andrew.w@covergenius.com>
2022-10-13 11:43:47 +1100

    Update python config for python 3.9

--- programs/python.nix
+++ programs/python.nix
@@ -7,13 +7,13 @@
 
   home.packages = with pkgs; [
     (
-      python38.withPackages (ps: [
-        ps.python-language-server
+      python39.withPackages (ps: [
+        ps.python-lsp-server
         ps.setuptools
         # type checking, import sorting and code formatting
-        ps.pyls-mypy
+        ps.pylsp-mypy
         ps.pyls-isort
-        ps.pyls-black
+        ps.python-lsp-black
       ])
     )
   ];
