c966378be9aaf276263f49cc05946b472d9804c3
Andrew Wright <andrew.wright502@gmail.com>
2024-11-30 15:27:06 +1100

    Update pre-commit and fix errors
    Also fix missing flag in install script

--- install.sh
+++ install.sh
@@ -5,10 +5,11 @@ mkdir -p ~/.config && cd ~/.config
 git clone https://github.com/aywrite/nix-config ./nixpkgs
 ln -s ~/.config/nixpkgs ~/nix-home
 
-sh <(curl https://nixos.org/nix/install)
+sh <(curl -L "https://nixos.org/nix/install")
 
 cd ~/.config/nix-home
 cp home-template.nix home.nix
 
 echo 'You should now install home-manager'
-mkdir -m 0755 -p /nix/var/nix/{profiles,gcroots}/per-user/$USER
+mkdir -p /nix/var/nix/{profiles,gcroots}/per-user/$USER
+chmod 0755 /nix/var/nix/{profiles,gcroots}/per-user/$USER
