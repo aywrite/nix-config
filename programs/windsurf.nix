4a601c091a447cf93751778be1d91f8c3f1a21ef
Andrew Wright <andrew.w@covergenius.com>
2024-12-14 11:37:18 +1100

    Add initial windsurf config

--- programs/windsurf.nix
+++ programs/windsurf.nix
@@ -1,72 +1,76 @@
-# windsurf.nix
 { config, pkgs, ... }:
 
 let
-  settingsPath = if pkgs.stdenv.isDarwin then
-    "Library/Application Support/Windsurf/User/settings.json"
-  else
-    ".config/windsurf/settings.json";
-in
-{
-  home.file.${settingsPath}.text = builtins.toJSON {
+  settings = {
     theme = {
       name = "solarized-dark"; # Match your vim theme
+    };
+    terminal = {
+      font = {
+        family = "FiraCode";
+        size = 12;
+      };
+      shell = {
+        program = "zsh";
+      };
+      defaultProfile = "zsh";
+    };
+    vim = {
+      enabled = true;
+      useSystemClipboard = true;
+      leader = "<Space>";
+      normalModeKeysNonRepeat = false; # Enable key repeat for normal mode
+      insertModeKeyBindings = [
+        {
+          before = [ "j" "k" ];
+          after = [ "<Esc>" ];
+        }
+      ];
+      normalModeKeyBindings = [
+        # File operations
+        {
+          before = [ "<leader>" "w" ];
+          commands = [ "workbench.action.files.save" ];
+        }
+        {
+          before = [ "<leader>" "q" ];
+          commands = [ "workbench.action.closeActiveEditor" ];
+        }
+        # Navigation
+        {
+          before = [ "<leader>" "gd" ];
+          commands = [ "editor.action.revealDefinition" ];
+        }
+        {
+          before = [ "<leader>" "gr" ];
+          commands = [ "editor.action.goToReferences" ];
+        }
+        {
+          before = [ "<leader>" "gi" ];
+          commands = [ "editor.action.goToImplementation" ];
+        }
+        # Search and replace
+        {
+          before = [ "<leader>" "f" ];
+          commands = [ "editor.action.formatDocument" ];
+        }
+        {
+          before = [ "<leader>" "r" ];
+          commands = [ "editor.action.rename" ];
+        }
+      ];
+      visualModeKeyBindings = [ ];
+      handleKeys = {
+        "<C-d>" = true;
+        "<C-f>" = false; # Let VSCode handle find
+        "<C-h>" = true;
+        "<C-w>" = false; # Let VSCode handle window management
+      };
     };
     editor = {
       font = {
         family = "FiraCode";
         size = 12;
-      };
-      vim = {
-        enabled = true;
-        useSystemClipboard = true;
-        leader = "<Space>";
-        insertModeKeyBindings = [
-          {
-            before = [ "j" "k" ];
-            after = [ "<Esc>" ];
-          }
-        ];
-        normalModeKeyBindings = [
-          # File operations
-          {
-            before = [ "<leader>" "w" ];
-            commands = [ "workbench.action.files.save" ];
-          }
-          {
-            before = [ "<leader>" "q" ];
-            commands = [ "workbench.action.closeActiveEditor" ];
-          }
-          # Navigation
-          {
-            before = [ "<leader>" "gd" ];
-            commands = [ "editor.action.revealDefinition" ];
-          }
-          {
-            before = [ "<leader>" "gr" ];
-            commands = [ "editor.action.goToReferences" ];
-          }
-          {
-            before = [ "<leader>" "gi" ];
-            commands = [ "editor.action.goToImplementation" ];
-          }
-          # Search and replace
-          {
-            before = [ "<leader>" "f" ];
-            commands = [ "editor.action.formatDocument" ];
-          }
-          {
-            before = [ "<leader>" "r" ];
-            commands = [ "editor.action.rename" ];
-          }
-        ];
-        visualModeKeyBindings = [ ];
-        handleKeys = {
-          "<C-d>" = true;
-          "<C-f>" = false; # Let VSCode handle find
-          "<C-h>" = true;
-          "<C-w>" = false; # Let VSCode handle window management
-        };
       };
       settings = {
         "editor.lineNumbers" = "relative";
@@ -81,22 +85,27 @@ in
         "files.insertFinalNewline" = true;
       };
     };
-    terminal = {
-      font = {
-        family = "FiraCode";
-        size = 12;
-      };
-      shell = {
-        program = "zsh";
-      };
-    };
     workbench = {
       colorTheme = "Solarized Dark";
       iconTheme = "material-icon-theme";
     };
-    window = {
-      zoomLevel = 0;
-      menuBarVisibility = "toggle";
-    };
   };
+
+  wslSettingsPath = ".local/share/windsurf/settings.json";
+  windowsSettingsPath = "AppData/Roaming/Windsurf/User/settings.json";
+  linuxSettingsPath = ".config/windsurf/settings.json";
+  darwinSettingsPath = "Library/Application Support/Windsurf/User/settings.json";
+
+  settingsJson = builtins.toJSON settings;
+in
+{
+  home.file =
+    if pkgs.stdenv.isDarwin then {
+      ${darwinSettingsPath}.text = settingsJson;
+    } else if config.role.isWSL then {
+      ${wslSettingsPath}.text = settingsJson;
+      ${windowsSettingsPath}.text = settingsJson;
+    } else {
+      ${linuxSettingsPath}.text = settingsJson;
+    };
 }
